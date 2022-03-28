c402f39c95e19badc8ac230436e7c99e4f059ecc
Joel Lee <lee.yi.jie.joel@gmail.com>
2022-03-28 17:41:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,19 @@
 import streamlit as st
-import psycopg2
-from urllib.parse import urlparse
+from supabase import create_client, Client
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 def init_connection():
-    result = urlparse(st.secrets["postgres"]["connstr"])
-    username = result.username
-    password = result.password
-    database = result.path[1:]
-    hostname = result.hostname
-    port = result.port
-    return psycopg2.connect(
-        database = database,
-        user = username,
-        password = password,
-        host = hostname,
-        port = port
-    )
-conn = init_connection()
+    url    = st.secrets["postgres"]["supabase_url"]
+    key    = st.secrets["postgres"]["supabase_key"]
+    return create_client(url, key)
+supabase = init_connection()
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-rows = run_query("SELECT * from countries;")
+def run_query():
+    return supabase.table("countries").select("*").execute()
+rows = run_query()
 # Print results.
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
