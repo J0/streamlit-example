39fead4d5da18bbe210c0ac4a95ac1f5223710be
Joel Lee <lee.yi.jie.joel@gmail.com>
2022-03-28 19:50:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,9 @@ def run_query():
 # Results are returned as [...results]
 rows = run_query()
 print(rows)
-print(rows.data[0])
+print(rows.data)
 
 # Print results.
-# for row in rows.data:
-#     st.write(f"Name of country is {row["name"]} and it is on:{row["continent"]}:")
+for row in rows.data:
+    st.write(row)
+    # st.write(f"Name of country is {row["name"]} and it is on:{row["continent"]}:")
